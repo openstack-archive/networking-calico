fe02c943166cbf2b8fbb683264179521b4766cc8
Alexander Saprykin <asaprykin@mirantis.com>
2016-11-02 14:09:44 +0100

    Allocate DHCP ports for agent
    Calico dhcp agent created dhcp interface with gateway IP address
    assigned.
    
    With this patch neutron server pre-allocates DHCP port,
    which allows to reserve single IP address shared between DHCP
    agents and distinct from gateway address.
    
    Neutron server allocates one DHCP port per network and deletes
    it if network doesn't have any subnet with DHCP enabled.
    
    Closes-Bug: #1541490
    Closes-Bug: #1625209
    Change-Id: Ib60394366d25ab5ffb51fe5827b0a273d3d71aee

--- networking_calico/tests/test_dhcp_agent.py
+++ networking_calico/tests/test_dhcp_agent.py
@@ -45,18 +45,19 @@ class TestFakePlugin(base.BaseTestCase):
         self.plugin = FakePlugin()
 
     def test_create(self):
-        port = self.plugin.create_dhcp_port({
-            'port': {'network_id': 'net-id-0'}
-        })
-        self.assertEqual({
-            'network_id': 'net-id-0',
-            'device_owner': 'network:dhcp',
-            'id': 'net-id-0',
-            'mac_address': '02:00:00:00:00:00'},
-            port)
+        self.assertRaises(
+            NotImplementedError, self.plugin.create_dhcp_port,
+            {'port': {'network_id': 'net-id-0'}})
+
+    def test_update(self):
+        self.assertRaises(
+            NotImplementedError, self.plugin.update_dhcp_port,
+            'port-id-0', {'port': {'network_id': 'net-id-0'}})
 
     def test_release(self):
-        self.plugin.release_dhcp_port('calico', 'dhcp')
+        self.assertRaises(
+            NotImplementedError, self.plugin.release_dhcp_port,
+            'calico', 'dhcp')
 
 
 class TestDhcpAgent(base.BaseTestCase):
@@ -329,6 +330,10 @@ class TestDhcpAgent(base.BaseTestCase):
                     'cidr': '2001:db8:1::/80',
                     'gateway_ip': '2001:db8:1::1'
                 }))
+            if datamodel_v1.DHCP_PORT_DIR in key:
+                etcd_response = mock.Mock()
+                etcd_response.leaves = []
+                return etcd_response
 
             eventlet.sleep(10)
             return None
@@ -519,7 +524,7 @@ class TestDhcpAgent(base.BaseTestCase):
             datamodel_v1.dir_for_host(socket.gethostname()) + '/workload'
         check_provided_args(agent.etcd, expected_key)
 
-        expected_key = datamodel_v1.SUBNET_DIR
+        expected_key = datamodel_v1.DHCP_DIR
         check_provided_args(agent.etcd.subnet_watcher, expected_key)
 
 
