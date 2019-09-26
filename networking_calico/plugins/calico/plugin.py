d18a8883016e0b05f322027d9183635017297025
Neil Jerram <neil@tigera.io>
2019-09-26 18:34:39 +0100

    Incorporate Extraroute_Db_mixin, as openstackclient assumes this
    See
    https://github.com/openstack/python-openstackclient/blob/master/openstackclient/network/v2/router.py#L52
    
    Also debug openstack router create, until we're sure about this.
    
    Change-Id: I692e30ebfb16a6cc73373834c49a4cd4cf1557ee

--- networking_calico/plugins/calico/plugin.py
+++ networking_calico/plugins/calico/plugin.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron.db import extraroute_db
 from neutron.db import l3_db
 from neutron.plugins.ml2.plugin import Ml2Plugin
 
@@ -23,7 +24,8 @@ from networking_calico.compat import log
 LOG = log.getLogger(__name__)
 
 
-class CalicoPlugin(Ml2Plugin, l3_db.L3_NAT_db_mixin):
+class CalicoPlugin(Ml2Plugin, l3_db.L3_NAT_db_mixin,
+                   extraroute_db.ExtraRoute_db_mixin):
 
     # These attributes specify whether the plugin supports or not
     # bulk/pagination/sorting operations. Name mangling is used in
